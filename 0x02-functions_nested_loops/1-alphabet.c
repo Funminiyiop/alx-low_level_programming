30a2c0581aa246045f0b57b13fe83811d3ebaf15
Funminiyiop <funminiyiop@gmail.com>
2023-06-20 22:23:07 -0700

    modify 1-alphabet.c file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,21 @@
 #include "main.h"
+
 /**
  * main - Entry point
  * Description: prints alphabet in lowercase,
  * Return: Always returns 0
  */
+
 void print_alphabet(void)
 {
 	char i;
 
-	for (i = 'a'; i <= 'z'; i++)
+	i = 'a';
+
+	while i <= 'z')
 	{
 		_putchar(i);
+		i++;
 	}
 	_putchar('\n');
 }
-
