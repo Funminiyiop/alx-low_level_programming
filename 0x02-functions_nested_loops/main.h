891b153845bdeebb488b6785276ee65f3128e878
Funminiyiop <funminiyiop@gmail.com>
2023-06-21 22:01:46 -0700

    adding 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,12 +2,7 @@
 #define main_h
 
 int _putchar(char);
-
-/**
- * print_alphabet - prints the alphabet in lowercase
- */
 void print_alphabet(void);
-
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
