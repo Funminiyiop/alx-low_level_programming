494e8ee03b55e6694faacd98596f69ae5ad22376
Funminiyiop <funminiyiop@gmail.com>
2023-07-07 21:38:01 -0700

    remaining task files on 0x0A-argc_argv

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print all argument passed to program each on a line
+ * @argc: argument counter
+ * @argv: pointer to array of arguments
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	while (i < argc)
+		printf("%s\n", argv[i++]);
+
+	return (0);
+}
