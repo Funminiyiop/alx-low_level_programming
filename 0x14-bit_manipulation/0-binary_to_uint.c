8f7ec6e4dd6343abdc60230ef8ca4bc010bfcca7
Funminiyi Peter <funminiyiop@gmail.com>
2023-09-29 16:16:03 -0700

    another modification on 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,10 +13,10 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int j;
 
 	j = 0;
+
 	if (!b)
-	{
 		return (0);
-	}
+
 	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] != '0' && b[i] != '1')
