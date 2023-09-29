9c96b611f5cf6e6b755a9e62b2e74301a7e9760d
Funminiyi Peter <funminiyiop@gmail.com>
2023-09-29 16:44:46 -0700

    adding task 1.1 files

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,8 +1,9 @@
-#ifndef MAIN_H 
+#ifndef MAIN_H
 #define MAIN_H
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
+unsigned long int _power(unsigned int base, unsigned int pow);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
