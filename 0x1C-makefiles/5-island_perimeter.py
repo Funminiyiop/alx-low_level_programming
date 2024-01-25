1f2cccd86dc9137853b0cf7e6a46d5506d06579f
Funminiyi Peter <funminiyiop@gmail.com>
2024-01-25 04:00:01 +0100

    correcting the island parameter file

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-"""
-gets Island Perimeter
-"""
+""" This module computes the perimeter of an island"""
+
 
 def island_perimeter(grid):
-    """actual function"""
+    """ island_perimeter:
+        Args: grid (list) = list of integers
+        Return: the perimeter of island described in grid
+    """
+
     p = 0
     for row in range(len(grid)):
         for col in range(len(grid[0])):
@@ -18,18 +21,3 @@ def island_perimeter(grid):
                 if col == (len(grid[0]) - 1) or grid[row][col + 1] == 0:
                     p += 1  # right
     return p
-
-
-# Method for rectangular island only, no odd shapes
-#    maxWidth = 0
-#    length = 0
-#    for i in range(len(grid)):
-#        width = 0
-#        for j in range(len(grid[0])):
-#            if grid[i][j] == 1:
-#                width += 1
-#        if width:
-#            length += 1
-#        if width > maxWidth:
-#            maxWidth = width
-#    return ((maxWidth + length) * 2)
